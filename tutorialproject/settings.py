e2719e56cae2ed68d10f4aff4162957a06ed64f0
abhinandtk.primalcodes <abhinandtk.primalcodes@gmail.com>
2025-02-18 11:41:49 +0530

    changed static path and delete staticfiles

--- tutorialproject/settings.py
+++ tutorialproject/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-(=i$$5i4hn4=-n##dtyjuo3=6prg88tkeuf_%+n7jm+kg&!$)c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['drflive-production.up.railway.app']
+ALLOWED_HOSTS = ['drflive-production.up.railway.app','127.0.0.1']
 
 
 
@@ -155,7 +155,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Directory where collectstatic will place files
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Directory where collectstatic will place files
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR,'static'),  # Example directory for static files
 # ]
