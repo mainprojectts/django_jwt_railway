a85f1e6b3ead1f0f533e3597ab144435df090ccd
abhinandtk.primalcodes <abhinandtk.primalcodes@gmail.com>
2025-02-18 12:09:20 +0530

    added statifiles

--- tutorialproject/settings.py
+++ tutorialproject/settings.py
@@ -155,7 +155,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Directory where collectstatic will place files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Directory where collectstatic will place files
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR,'static'),  # Example directory for static files
 # ]
